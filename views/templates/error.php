a9194f17dcbf07c1ecc3210a2d200c6044617a5d
Julien Bernard <julienbernard@Juliens-MacBook-Pro.local>
2025-12-14 17:05:20 +0100

    Page Erreur

--- views/templates/error.php
+++ views/templates/error.php
@@ -1,1 +1,5 @@
-<h2>ERREUR</h2>
+<div class="error-page">
+    <h2 class="error-title">OUPS ! ERREUR</h2>
+    <p class="error-text">Cette page n'existe pas, veuillez retourner sur la page d'accueil</p>
+    <a href="index.php" class="error-link error-text">Retour à l'accueil</a>
+</div>
